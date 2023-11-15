72727613888d453462d3715c27b9b06d1035a725
niuheshui <niuheshui@gmail.com>
2023-11-15 16:11:59 +0800

    submit lab10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,10 +1,36 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+            (if (< num1 num2)
+                -1
+                (if (> num1 num2)
+                    1 
+                    0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (over-or-under_2 num1 num2)
+            (cond
+                ((> num1 num2) 1)
+                ((< num1 num2) -1)
+                ((= num1 num2) 0)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+            (define (adder x) 
+                        (+ x num))
+            adder)
+        
+(define (make-adder-lambda num)
+            (lambda(x) (+ x num)))
+
+
+(define (composed f g) 
+            (lambda(x) (f (g x))))
+
+(define (repeat f n) 
+            (if (<= n 1)
+                    f
+                (composed 
+                    (repeat f (- n 1))
+                    f)))
+            
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +42,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+            (if (= 0 (modulo a b))
+                    b
+                    (gcd b (modulo a b))))
