c4b65229cc19597fdbf0a29f6bf074190fa3d72b
niuheshui <niuheshui@gmail.com>
2023-11-28 11:56:44 +0800

    store ...

--- hw09/hw09.scm
+++ hw09/hw09.scm
@@ -1,7 +1,29 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+;scm> (curry-cook '(a) 'a)
+;(lambda (a) a)
+;scm> (curry-cook '(x y) '(+ x y))
+;(lambda (x) (lambda (y) (+ x y)))
+
+(define (curry-cook formals body) 
+    (if (null? (cdr formals))
+        `(lambda ,formals ,body)
+        `(lambda ,(cons (car formals) nil) ,(curry-cook (cdr formals) body))))
+
+
+;scm> (define three-curry (lambda (x) (lambda (y) (lambda (z) (+ x (* y z)))) ))
+;three-curry
+;scm> (define eat-two (curry-consume three-curry '(1 2))) ; pass in only two arguments, return should be a one-arg lambda function!
+;eat-two
+;scm> eat-two
+;(lambda (z) (+ x (* y z)))
+;scm> (eat-two 3) ; pass in the last argument; 1 + (2 * 3)
+;7
+;scm> (curry-consume three-curry '(1 2 3)) ; all three arguments at once
+;7
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+      curry
+      (curry-consume (curry (car args)) (cdr args))))
 
 (define-macro (switch expr cases)
   (switch-to-cond (list 'switch expr cases)))
