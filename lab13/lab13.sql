57b009a9fe8f1b3134367c8900ba587846944325
niuheshui <niuheshui@gmail.com>
2023-11-29 17:17:50 +0800

    submit lab13

--- lab13/lab13.sql
+++ lab13/lab13.sql
@@ -2,20 +2,31 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color = "blue" AND pet = "dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song FROM students WHERE color = "blue" AND pet = "dog";
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest 
+    FROM students 
+    GROUP BY smallest 
+    HAVING count(*) < 2;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT first.pet, first.song, first.color, second.color
+    FROM students as first, students as second
+    WHERE first.time != second.time AND
+          first.pet = second.pet AND
+          first.song = second.song;
+
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT students.seven 
+    FROM students, numbers
+    WHERE students.time = numbers.time 
+          AND numbers.'7' = 'True' 
+          AND students.smallest <= 7;
